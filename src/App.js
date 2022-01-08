379e99f852805f1345f8f267c1f04b40b69f1b56
Michael Schodin <michael.schodin@gmail.com>
2022-01-08 00:29:35 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { ReactText } from 'react';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* components here */}
+        <p className="example1">example 1</p>
+        <p className="example2">example 2</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Nav.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+
+function NavBar(props) {
+    const [loginShow, setLoginShow] = React.useState(false);
+    const [signupShow, setSignupShow] = React.useState(false);
+    const [forgotPasswordShow, setForgotPasswordShow] = React.useState(false);
+
+    function logout() {
+        let token = $('meta[name=csrf-token]').attr('content');
+        fetch(props.logout_route, {
+            method: 'delete',
+            headers: {
+                'X-CSRF-Token': token
+            }
+        }).then(() => {
+            window.location.reload();
+        })
+    }
+
+    let button;
+    let email;
+    let profile_url;
+    if(props.signed_in) {
+        email = <div style={{marginRight: '15px', color: 'white'}}> {props.current_user} </div>
+        button = <Button onClick={logout} variant={'dark'}>Logout</Button>
+        profile_url = props.user_id
+    } else {
+        email = "";
+        button = <Button onClick={() => setLoginShow(true)} variant={'dark'}>Login</Button>
+        profile_url = 1
+    }
+
+    return (
+        <>
+            <Navbar bg="primary">
+                <Navbar.Brand href="/home">
+                    <img
+                        src={sg_logo}
+                        width="50"
+                        height="50"
+                        className="d-inline-block align-top logo"
+                        alt="Scrub Garden Logo"
+                    />
+                </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href={"/schedule"}>Schedule</Nav.Link>
+                    <Nav.Link href={"/team"}>Teams</Nav.Link>
+                    <Nav.Link href={"/profile/" + profile_url}>Profile</Nav.Link>
+                    <Nav.Link href={"/messages"}>Messages</Nav.Link>
+                    <Nav.Link href={"/your_scrims"}>Your Scrims</Nav.Link>
+                </Nav>
+                {email}
+                {button}
+            </Navbar>
+            <LoginModal
+                show={loginShow}
+                onHide={(showSignup, showForgotPassword) => {
+                    setLoginShow(false);
+                    setSignupShow(showSignup);
+                    setForgotPasswordShow(showForgotPassword)
+                }}
+                login_route={props.login_route}
+                status_route={props.status_route}
+            />
+            <SignUpModal
+                show={signupShow}
+                onHide={(showLogin) => {
+                    setSignupShow(false);
+                    setLoginShow(showLogin);
+                }}
+                signup_route={props.signup_route}
+                exists_route={props.exists_route}
+                create_profile_route={props.create_profile_route}
+            />
+            <ForgotPasswordModal
+                show={forgotPasswordShow}
+                onHide={(showLogin) => {
+                    setForgotPasswordShow(false);
+                    setLoginShow(showLogin)
+                }}
+                reset_password={props.reset_password}
+            />
+        </>
+    );
+};
+
+export default NavBar;
