e103d4e0df9733afcd1fca9aa19f2e4011d1e860
Michael Schodin <michael.schodin@gmail.com>
2022-01-08 21:30:31 -0600

    commit before react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { ReactText } from 'react';
+import React from 'react';
+import NavBar from './Nav';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* components here */}
-        <p className="example1">example 1</p>
-        <p className="example2">example 2</p>
-      </header>
+        <NavBar/>
     </div>
   );
 }
