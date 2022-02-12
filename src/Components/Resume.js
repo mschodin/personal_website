f525d6792b739d3345b09f97f0cd968390a398f7
Michael Schodin <michael.schodin@gmail.com>
2022-02-11 19:23:06 -0600

    Removed resume download

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,9 +5,7 @@ function Resume(props) {
     return (
         <>
             <div className="resume-container">
-                <a download href='../Assets/MichaelSchodinResume.pdf'>
-                    <img className="resume-image" src={require('../Assets/MichaelSchodinResume2022.png')} alt="Resume Path not found"/>    
-                </a>
+                <img className="resume-image" src={require('../Assets/MichaelSchodinResume2022.png')} alt="Resume Path not found"/>    
             </div>
         </>
     );
