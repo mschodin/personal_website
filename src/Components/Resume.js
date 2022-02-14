4aff02b4ca1efcbfc0d630d9c86db216dd496bb0
Michael Schodin <michael.schodin@gmail.com>
2022-02-14 13:28:40 -0600

    Download resume on click

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Resume.css';
+import resumePDF from '../Assets/MichaelSchodinResume.pdf';
 
 function Resume(props) {
     return (
         <>
             <div className="resume-container">
-                <img className="resume-image" src={require('../Assets/MichaelSchodinResume2022.png')} alt="Resume Path not found"/>    
+                <a download href={resumePDF}>
+                    <img className="resume-image" src={require('../Assets/MichaelSchodinResume2022.png')} alt="Resume Path not found"/>    
+                </a>
             </div>
         </>
     );
